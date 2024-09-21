c5f43e5f38b641cd8039d137db0412710b461fcd
Rafiou Sitou <rafiou.sitou@sicoob.com.br>
2024-09-20 23:31:44 -0300

    Initial commit

---
+++ src/main/java/br/com/rafdev/training/demojenkinssonaqube/DemoJenkinsSonaqubeApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafdev.training.demojenkinssonaqube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenkinsSonaqubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJenkinsSonaqubeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/rafdev/training/demojenkinssonaqube/DemoJenkinsSonaqubeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafdev.training.demojenkinssonaqube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinsSonaqubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
